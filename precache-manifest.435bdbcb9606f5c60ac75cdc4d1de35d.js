876a026ea3438457c63428cfb733ee459217dfef
ljnferreira <ljnferreira@gmail.com>
2020-04-26 18:18:41 -0300

    Updates

---
+++ precache-manifest.435bdbcb9606f5c60ac75cdc4d1de35d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72f5626345cdc16150ec6da89e3a3f06",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4c5829fbd2c487236da",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "24bed3ad7d3aa732e2e2",
+    "url": "/static/js/2.66d5e210.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.66d5e210.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4c5829fbd2c487236da",
+    "url": "/static/js/main.0bf65a75.chunk.js"
+  },
+  {
+    "revision": "19d4da64ec0b429187a9",
+    "url": "/static/js/runtime-main.919624c7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
